2d0440ed6a9f2f89498e9eb3c25604b2ed56ffe7
prithvi <prithvibhola08@gmail.com>
2018-09-02 18:33:43 +0530

    Marvel Character API response Models and ResponseModels created

--- app/src/main/java/prithvi/io/mvvmpagination/data/models/Models.kt
+++ app/src/main/java/prithvi/io/mvvmpagination/data/models/Models.kt
@@ -1,5 +1,7 @@
 package prithvi.io.mvvmpagination.data.models
 
+import com.squareup.moshi.Json
+
 data class Response<out T>(
         val status: Status,
         val data: T? = null,
@@ -13,3 +15,76 @@ data class Response<out T>(
         fun <T> error(error: Throwable): Response<T> = Response(Status.ERROR, null, error)
     }
 }
+
+data class Character(
+        @Json(name = "id") val id: Long,
+        @Json(name = "name") val name: String,
+        @Json(name = "description") val description: String,
+        @Json(name = "modified") val modified: String,
+        @Json(name = "thumbnail") val thumbnail: Thumbnail,
+        @Json(name = "resourceURI") val resourceURI: String,
+        @Json(name = "comics") val comics: Comics,
+        @Json(name = "series") val series: Series,
+        @Json(name = "stories") val stories: Stories,
+        @Json(name = "events") val events: Events,
+        @Json(name = "urls") val urls: List<Urls>
+)
+
+data class Thumbnail(
+        @Json(name = "path") val path: String,
+        @Json(name = "extension") val extension: String
+)
+
+data class Comics(
+        @Json(name = "available") val available: Int,
+        @Json(name = "collectionURI") val collectionURI: String,
+        @Json(name = "items") val items: List<ComicsItems>,
+        @Json(name = "returned") val returned: Int
+)
+
+data class ComicsItems(
+        @Json(name = "resourceURI") val resourceURI: String,
+        @Json(name = "name") val name: String
+)
+
+data class Series(
+        @Json(name = "available") val available: Int,
+        @Json(name = "collectionURI") val collectionURI: String,
+        @Json(name = "items") val items: List<SeriesItems>,
+        @Json(name = "returned") val returned: Int
+)
+
+data class SeriesItems(
+        @Json(name = "resourceURI") val resourceURI: String,
+        @Json(name = "name") val name: String
+)
+
+data class Stories(
+        @Json(name = "available") val available: Int,
+        @Json(name = "collectionURI") val collectionURI: String,
+        @Json(name = "items") val items: List<StoriesItems>,
+        @Json(name = "returned") val returned: Int
+)
+
+data class StoriesItems(
+        @Json(name = "resourceURI") val resourceURI: String,
+        @Json(name = "name") val name: String,
+        @Json(name = "type") val type: String
+)
+
+data class Events(
+        @Json(name = "available") val available: Int,
+        @Json(name = "collectionURI") val collectionURI: String,
+        @Json(name = "items") val items: List<EventsItems>,
+        @Json(name = "returned") val returned: Int
+)
+
+data class EventsItems(
+        @Json(name = "resourceURI") val resourceURI: String,
+        @Json(name = "name") val name: String
+)
+
+data class Urls(
+        @Json(name = "type") val type: String,
+        @Json(name = "url") val url: String
+)
--- app/src/main/java/prithvi/io/mvvmpagination/data/models/ResponseModels.kt
+++ app/src/main/java/prithvi/io/mvvmpagination/data/models/ResponseModels.kt
@@ -1,1 +1,21 @@
 package prithvi.io.mvvmpagination.data.models
+
+import com.squareup.moshi.Json
+
+data class CharactersResponse(
+        @Json(name = "code") val code: Int,
+        @Json(name = "status") val status: String,
+        @Json(name = "copyright") val copyright: String,
+        @Json(name = "attributionText") val attributionText: String,
+        @Json(name = "attributionHTML") val attributionHTML: String,
+        @Json(name = "etag") val etag: String,
+        @Json(name = "data") val data: CharactersDataResponse
+)
+
+data class CharactersDataResponse(
+        @Json(name = "offset") val offset: Int,
+        @Json(name = "limit") val limit: Int,
+        @Json(name = "total") val total: Int,
+        @Json(name = "count") val count: Int,
+        @Json(name = "results") val results: List<Character>
+)
